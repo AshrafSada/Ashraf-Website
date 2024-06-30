671bfd07eb097a9890e7b75137162e7aa086fd2e
Ashraf Sada <ashraf.sada@outlook.com>
2024-06-30 15:58:40 +0300

    - Added hero Section

---
+++ app/ThemeProvider.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { ThemeProvider as NextThemeProvider } from 'next-themes'
+import { ThemeProviderProps } from 'next-themes/dist/types'
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+   return <NextThemeProvider {...props}>{children}
+   </NextThemeProvider>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+import Grid from '@/Components/Grid'
+import Hero from '@/Components/Hero'
+import { FloatingNav } from '@/Components/ui/FloatingNavbar'
 import React from 'react'
+import { FaContao, FaHome, FaInfo, FaRProject } from 'react-icons/fa'
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>Home</h1>
-      <p>Welcome to my website</p>
-    </div>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={
+          [
+            { name: 'Home', link: '/', icon: <FaHome /> },
+            { name: 'About', link: '#about', icon: <FaInfo /> },
+            { name: 'Projects', link: '#projects', icon: <FaRProject /> },
+            { name: 'Contact', link: '#contact', icon: <FaContao /> }
+          ]
+        } />
+        <Hero />
+        <Grid />
+      </div>
+    </main>
   )
 }
